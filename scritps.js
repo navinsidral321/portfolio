0984fd814d6f25a54ace30e9bca73c38a9721571
navinsidral321 <72349125+navinsidral321@users.noreply.github.com>
2024-12-05 15:25:34 +0530

    release commit

--- scritps.js
+++ scritps.js
@@ -4,3 +4,38 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+// updateBody();
+
+function updateBody() {
+  // if (inputEl.checked) {
+  //   bodyEl.style.background = "black";
+  // } else {
+  //   bodyEl.style.background = "white";
+  // }
+
+  var theme = document.getElementsByTagName('link')[0]; 
+  
+            // Change the value of href attribute  
+            // to change the css sheet. 
+            if (theme.getAttribute('href') == 'style.css') { 
+                theme.setAttribute('href', 'style1.css'); 
+            } else { 
+                theme.setAttribute('href', 'style.css'); 
+            } 
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
